013a0c3b1943c1f83ae074f423c48dee8171c132
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-10-02 20:15:11 +0530

    created mongodb

---
+++ Mongodb.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+N="\e[0m"
+Y="\e[33m"
+G="\e[32m"
+
+
+if [ $USERID -ne 0 ];
+then 
+    echo -e "$R ERROR :: Run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then 
+        echo -e "Installing $2 ... $R FAILURE $N"
+    else
+        echo -e "installing $2 ... $G SUCCESS $N"
+    fi
+}
+
+
+
