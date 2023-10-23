39cdccec0272901072cafb4342a8b0e6ae77f9fb
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-10-23 20:29:52 +0530

    created createec2

---
+++ createec2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#now we previous discussed about array same way just giving a variable
+NAMES=("Web" "mongodb" "catlaogue" "cart" "user" "redis" "mysql" "shipping" "rabbitmq" "payment" "dispatch")
+        
+
+For i in "${NAMES[@]}"  
+do 
+    echo "ALL NAMES: $i"
+done 
